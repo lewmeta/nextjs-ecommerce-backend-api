d862b63cc55bc66ae032dc389463af3b57646b71
lewmeta <dev.lewismeta@gmail.com>
2024-09-18 17:31:31 +0300

    added a prisma model for the ecommerce

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -33,8 +33,8 @@ export async function POST(
         await db.store.create({
             data: {
                 name,
-                userId: user.id
-            }
+                userId: user.id,
+            },
         });
 
         return NextResponse.json({ success: "Store created successfully!" }, { status: 200 });
