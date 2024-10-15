1b0b4a9387d7021892cd9fc8c53f8317043b61d4
lewmeta <dev.lewismeta@gmail.com>
2024-10-15 17:21:58 +0300

    updated the design with cards

--- src/app/(routes)/[storeId]/vendor/products/[productId]/_components/category-form.tsx
+++ src/app/(routes)/[storeId]/vendor/products/[productId]/_components/category-form.tsx
@@ -21,6 +21,7 @@ import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Combobox } from "@/components/ui/combobox";
 import { useToast } from "@/hooks/use-toast";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface CategoryFormProps {
     initialData: Product;
@@ -78,64 +79,70 @@ export const CategoryForm = ({
 
 
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4 dark:bg-gray-800">
-            <div className="font-medium flex items-center justify-between">
-                Product Category
-                <Button
-                    onClick={toggleEdit}
-                    variant={'ghost'}
-                >
-                    {isEditing ? (
-                        <>Cancel</>
-                    ) : (
-                        <>
-                            <Pencil className="h-4 w-4 mr-2" />
-                            Edit category
-                        </>
-                    )}
-                </Button>
-            </div>
-            {!isEditing && (
-                <p className={cn(
-                    'text-sm mt-2',
-                    !initialData.categoryId && 'text-slate-800 italic'
-                )}>
-                    {selectOption?.label || "No category"}
-                </p>
-            )}
-            {isEditing && (
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-4 mt-4"
-                    >
-                        <FormField
-                            control={form.control}
-                            name="categoryId"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <Combobox
-                                            options={options}
-                                            disabled={isSubmitting}
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <div className="flex items-center gap-x-2">
-                            <Button 
-                            disabled={isSubmitting}
-                            type="submit"
+        <Card className="mt-5">
+            <CardHeader>
+                <CardTitle>
+                    Add Product Category
+                </CardTitle>
+                <CardDescription>
+                    Add a product category from the dropdown menu option.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                {!isEditing && (
+                    <div className={cn("flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors ring-1", !initialData.categoryId && 'italic')}>
+                        {selectOption?.label || "No category"}
+                    </div>
+                )}
+                {isEditing && (
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                        >
+                            <FormField
+                                control={form.control}
+                                name="categoryId"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Combobox
+                                                options={options}
+                                                disabled={isSubmitting}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button
+                                disabled={isSubmitting}
+                                type="submit"
+                                className="mt-3"
                             >
                                 Save
                             </Button>
-                        </div>
-                    </form>
-                </Form>
-            )}
-        </div>
+                        </form>
+                    </Form>
+                )}
+            </CardContent>
+            <CardFooter>
+                <div className="font-medium flex items-center justify-between">
+                    <Button
+                        onClick={toggleEdit}
+                        variant={'ghost'}
+                    >
+                        {isEditing ? (
+                            <>Cancel</>
+                        ) : (
+                            <>
+                                <Pencil className="h-4 w-4 mr-2" />
+                                Edit category
+                            </>
+                        )}
+                    </Button>
+                </div>
+            </CardFooter>
+        </Card>
     )
 }
