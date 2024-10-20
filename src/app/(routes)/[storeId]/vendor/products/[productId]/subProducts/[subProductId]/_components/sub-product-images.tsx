c9dfb458280fa24a9b47f1677b5d7f6dc509acb8
lewmeta <dev.lewismeta@gmail.com>
2024-10-20 15:58:34 +0300

    slight update on the images component

--- src/app/(routes)/[storeId]/vendor/products/[productId]/subProducts/[subProductId]/_components/sub-product-images.tsx
+++ src/app/(routes)/[storeId]/vendor/products/[productId]/subProducts/[subProductId]/_components/sub-product-images.tsx
@@ -105,7 +105,7 @@ export const SubProductImages = ({
                             <div className="flex items-center gap-3 w-full h-full"
                             >
                                 {initialData.images.slice(0, 2).map((image, index) => (
-                                    <div className="w-full h-[500px]  relative">
+                                    <div className="w-full h-[400px]  relative">
                                         <Image
                                             key={index}
                                             alt={`Product image ${index + 1}`}
